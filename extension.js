d8f9093375eaf0f06ed672461644cadb7fd55945
Alfred Birketvedt <alfredbirk@gmail.com>
2019-10-24 20:57:28 +0200

    Make extension

--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -19,9 +18,66 @@ function activate(context) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
+		console.log("lol")
+		const x = require('@emmetio/html-matcher');
+	
+		// const content = '<div><a href="http://emmet.io">Example</a></div>';
+	
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		
+		let selection = editor.selection;
+		let active = selection.active
 
+		const offset = editor.document.offsetAt(active)
+
+		console.log('active', active);
+		console.log('offset', offset);
+		// Find tag pair at character 35
+		const content = editor.document.getText();
+		const tag = x.default(content, offset+1);
+	
+		console.log('tag', tag)
+
+		const p1 = editor.document.positionAt(tag.open[0])
+		const p2 = editor.document.positionAt(tag.open[1])
+		const p3 = editor.document.positionAt(tag.close[0])
+		const p4 = editor.document.positionAt(tag.close[1])
+
+
+
+		const range1 = new vscode.Range(p1,p2);
+		const range2 = new vscode.Range(p3,p4);
+		console.log('range1:', range1);
+
+		const line1 = editor.document.lineAt(p1.line);
+		const line2 = editor.document.lineAt(p3.line);
+		console.log('line1:', line1);
+
+		const removeLine1 = line1.firstNonWhitespaceCharacterIndex === p1.character
+		const removeLine2 = line2.firstNonWhitespaceCharacterIndex === p3.character
+
+		editor.edit(function x(editBuilder) {
+			if (removeLine1) {
+				editBuilder.delete(line1.rangeIncludingLineBreak);
+			}
+			if (removeLine2) {
+				editBuilder.delete(line2.rangeIncludingLineBreak);
+			}
+		})
+	
+
+		// editor.edit(function x(editBuilder) {
+		// 	editBuilder.delete(range1)
+		// 	editBuilder.delete(range2)
+		// })
+
+		// console.log('editor.document.getText()', editor.document.getText());
+	
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World123456!');
+		vscode.window.showInformationMessage(tag.name);
 	});
 
 	context.subscriptions.push(disposable);
